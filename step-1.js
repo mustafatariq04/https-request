c1361dee4c3c609d623bc23533dfa068b15a8103
Mustafa Tariq <mustafatariq04@gmail.com>
2019-06-03 15:57:46 +0000

    step-1 of the https-request exercise completed

--- step-1.js
+++ step-1.js
@@ -1,3 +1,5 @@
+var https = require('https');
+
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
@@ -5,6 +7,19 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
 
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length + '\n');
+      console.log('Chunk Received :', data + "\n" + "\n" + "\n" + "\n");
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
 }
+
+getAndPrintHTMLChunks();
